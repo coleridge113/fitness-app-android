e9c46e7e126943ad9bb8132c6669264d4e431eca
Jose Luna <josemanuelmluna@gmail.com>
2025-08-10 16:55:27 +0800

    added hilt application

---
+++ app/src/main/java/com/example/fitness_app_as/application/FitnessApplication.kt
@@ -0,0 +1,7 @@
+package com.example.fitness_app_as.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FitnessApplication : Application()
