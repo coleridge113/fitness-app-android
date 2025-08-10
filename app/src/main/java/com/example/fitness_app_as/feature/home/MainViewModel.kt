5bc3f7edc35e1e631504826bebfeb310241b2d7a
Jose Luna <josemanuelmluna@gmail.com>
2025-08-10 17:29:22 +0800

    refactored to make use of states, shared flow, and viewmodels

--- app/src/main/java/com/example/fitness_app_as/feature/home/MainViewModel.kt
+++ app/src/main/java/com/example/fitness_app_as/feature/home/MainViewModel.kt
@@ -1,5 +1,27 @@
 package com.example.fitness_app_as.feature.home
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fitness_app_as.network.exercise.ExerciseRemoteSource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val exerciseRemoteSource: ExerciseRemoteSource
+) : ViewModel() {
+
+    private val mutableMainState: MutableSharedFlow<MainState> = MutableSharedFlow()
+    val mainState = mutableMainState.asSharedFlow()
+
+    fun getExerciseItems() {
+        viewModelScope.launch {
+            val exercises = exerciseRemoteSource.getExerciseItems()
+            mutableMainState.emit(MainState.LoadExercises(exercises))
+        }
+    }
 
 }
