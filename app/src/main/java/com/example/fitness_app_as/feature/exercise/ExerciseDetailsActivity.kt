a82c4a1e4a40e783aa292a68755c3c6d401bc68d
Jose Luna <josemanuelmluna@gmail.com>
2025-08-11 14:51:27 +0800

    added back button functionality

--- app/src/main/java/com/example/fitness_app_as/feature/exercise/ExerciseDetailsActivity.kt
+++ app/src/main/java/com/example/fitness_app_as/feature/exercise/ExerciseDetailsActivity.kt
@@ -3,7 +3,6 @@ package com.example.fitness_app_as.feature.exercise
 import android.os.Bundle
 import android.text.Html
 import android.text.method.LinkMovementMethod
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitness_app_as.databinding.ActivityExerciseDetailsBinding
 import com.example.fitness_app_as.domain.Exercise
@@ -16,6 +15,11 @@ class ExerciseDetailsActivity : AppCompatActivity() {
         binding = ActivityExerciseDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupView()
+        setupListeners()
+    }
+
+    private fun setupView() {
         val exercise = intent.getSerializableExtra("exercise", Exercise::class.java)
 
         exercise?.let {
@@ -27,10 +31,17 @@ class ExerciseDetailsActivity : AppCompatActivity() {
 
                 val link = "https://www.youtube.com/results?search_query=" + it.name.split(" ").joinToString("+")
                 val htmlText = "<a href=\"$link\">${it.name}</a>"
-                Log.d("ExerciseActivity", htmlText)
                 ytLink.text = Html.fromHtml(htmlText, Html.FROM_HTML_MODE_COMPACT)
                 ytLink.movementMethod = LinkMovementMethod.getInstance()
 
+            }
+        }
+    }
+
+    private fun setupListeners() {
+        with(binding){
+            toolbar.backButton.setOnClickListener {
+                onBackPressedDispatcher.onBackPressed()
             }
         }
     }
