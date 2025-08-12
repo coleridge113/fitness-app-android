ba89d823ca7486041488adb4c40bd04b547346b8
Jose Luna <josemanuelmluna@gmail.com>
2025-08-12 17:04:01 +0800

    added create playlist remote source

--- app/src/main/java/com/example/fitness_app_as/network/playlist/PlaylistRemoteSource.kt
+++ app/src/main/java/com/example/fitness_app_as/network/playlist/PlaylistRemoteSource.kt
@@ -15,4 +15,12 @@ class PlaylistRemoteSource @Inject constructor(
             if (response.isSuccessful) response.body() ?: emptyList() else emptyList()
         }
     }
+
+    suspend fun createPlaylist(playlist: Playlist): Boolean {
+        return withContext(Dispatchers.IO) {
+            val response = playlistService.createPlaylist(playlist)
+            response.isSuccessful
+        }
+
+    }
 }
